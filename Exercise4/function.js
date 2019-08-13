e71aff1ea1f2bcbe131d36666972a3ebd2a23b2b
unknown <eiphyothein@gmail.com>
2019-08-13 11:01:39 +0630

    manage folder

--- Exercise4/function.js
+++
@@ -1,9 +0,0 @@
-function birthDay(name) {
-    console.log("\nHappy birthday to you.");
-    console.log("Happy birthday to you.");
-    console.log("Happy birthday dear " + name + ".");
-    console.log("Happy birthday to you.");
-}
-
-birthDay("Ye Tun");
-birthDay("Thura Ye Tun")
