7d3515cc31824f407d83a7fe6e46c6ebcf128ec3
unknown <eiphyothein@gmail.com>
2019-08-20 14:32:27 +0630

    add data-jsonp folder to eptSupplement of Assignment8

---
+++ jQuery/Assignment8/eptSupplement/data-jsonp/js/data-jsonp.js
@@ -0,0 +1,15 @@
+//3. function that process the JSON data from the server sends. eg. (data)
+
+function showEvents(data) {                           
+  var newContent = '';
+  //create html code from JSON data                  
+  for (var i = 0; i < data.events.length; i++) {
+    newContent += `<div class="event"><img src="${data.events[i].map} alt="${data.events[i].location}" />`;
+    newContent += `<p><b>${data.events[i].location}</b><br>`;
+    newContent += `${data.events[i].date}</p></div>`;
+  }
+
+
+  //updating page
+document.getElementById('content').innerHTML = newContent;
+}
