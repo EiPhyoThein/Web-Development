0267b15760f6f9632e72668f133ed11c7f15f091
unknown <eiphyothein@gmail.com>
2019-08-20 14:33:58 +0630

    mend

--- jQuery/Assignment8/eptSupplement/data-jsonp/js/data-jsonp.js
+++ jQuery/Assignment8/eptSupplement/data-jsonp/js/data-jsonp.js
@@ -4,7 +4,7 @@ function showEvents(data) {
   var newContent = '';
   //create html code from JSON data                  
   for (var i = 0; i < data.events.length; i++) {
-    newContent += `<div class="event"><img src="${data.events[i].map} alt="${data.events[i].location}" />`;
+    newContent += `<div class="event"><img src="${data.events[i].map}" alt="${data.events[i].location}" />`;
     newContent += `<p><b>${data.events[i].location}</b><br>`;
     newContent += `${data.events[i].date}</p></div>`;
   }
