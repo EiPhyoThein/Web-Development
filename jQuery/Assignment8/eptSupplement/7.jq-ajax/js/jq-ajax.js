e10b4cf773498c3212328839b1fd6ac53a7a8cc9
unknown <eiphyothein@gmail.com>
2019-08-28 16:59:26 +0630

    add comment to 7.jq-ajax

--- jQuery/Assignment8/eptSupplement/7.jq-ajax/js/jq-ajax.js
+++ jQuery/Assignment8/eptSupplement/7.jq-ajax/js/jq-ajax.js
@@ -11,21 +11,31 @@ $('nav a').on('click', function(e) {
 
   $('nav a.current').removeClass('current');                // Update links
   $(this).addClass('current');
+  
+  //when click on link, clear all tags from #container
   $('#container').remove();                                 // Remove content
 
+  //using jQuery and ajax
   $.ajax({
     type: "GET",                                            // GET or POST
     url: url,                                               // Path to file
-    timeout: 2000,                                          // Waiting time
+    timeout: 2000, 
+    
+    // Waiting time
     beforeSend: function() {                                // Before Ajax 
       $content.append('<div id="load">Loading</div>');      // Load message
     },
+    //all condition(success or fail), remove the #load div
     complete: function() {                                  // Once finished
       $('#load').remove();                                  // Clear message
     },
+
+    //if success, tags inside the link file , will insert into the #content tag 
     success: function(data) {                               // Show content
       $content.html( $(data).find('#container') ).hide().fadeIn(400);
     },
+
+    //if fail, recreate container tag and show message.
     error: function() {                                     // Show error msg 
       $content.html('<div class="container">Please try again soon.</div>');
     }
