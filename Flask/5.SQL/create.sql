64a2168be158855f9e8609607fd50c831965ef1d
Ei Phyo Thein <eiphyothein@gmail.com>
2019-12-13 15:04:32 +0630

    SQL

--- Flask/5.SQL/create.sql
+++ Flask/5.SQL/create.sql
@@ -4,3 +4,9 @@ CREATE TABLE flights(
     destination VARCHAR NOT NULL,
     duration INTEGER NOT NULL
 );
+
+CREATE TABLE passengers(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    flight_id INTEGER REFERENCES flights
+);
--- Flask/5.SQL/createTable.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE passengers(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    flight_id INTEGER REFERENCES flights
-);
