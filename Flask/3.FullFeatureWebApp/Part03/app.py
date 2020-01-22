9e84e37ee4e995b7de5c495c0045386877abaf80
Ei Phyo Thein <eiphyothein@gmail.com>
2020-01-22 11:58:48 +0630

    Page17 Full Featured Web App

--- Flask/3.FullFeatureWebApp/Part03/app.py
+++ Flask/3.FullFeatureWebApp/Part03/app.py
@@ -34,3 +34,22 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html",title="About")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title="Register",form=form)
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=="admin@blog.com" and form.password.data=="password":
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+    return render_template('login.html',title="Login",form=form)
