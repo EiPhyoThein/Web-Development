8c0034d684e721c7b068005b16f7f49388e11b46
Ei Phyo Thein <eiphyothein@gmail.com>
2020-02-22 11:13:49 +0630

    after Taungoo, end fullfeature

--- Flask/3.FullFeatureWebApp/Part08/flaskblog/routes.py
+++ Flask/3.FullFeatureWebApp/Part08/flaskblog/routes.py
@@ -1,36 +1,17 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect,request
+from flask import render_template, url_for, flash, redirect,request,abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm 
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts=[
-    {
-        'author': 'Ei Phyo Thein',
-        'title': 'Post 1',
-        'content': 'En eye for an eye, a tooth for a tooth.',
-        'date_posted': 'January 23, 2020'
-    },
-    {
-        'author': 'U Chit Thein',
-        'title': 'Post 2',
-        'content': 'Nothings is free. ',
-        'date_posted': 'January 23, 2020'
-    },
-    {
-        'author': 'Ko Zin Ko Tun',
-        'title': 'Post 3',
-        'content': 'Work Perfect. ',
-        'date_posted': 'January 23, 2020'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts=Post.query.all()
     return render_template("home.html",posts=posts)
 
 @app.route("/about")
@@ -58,7 +39,6 @@ def login():
     form=LoginForm()
     if form.validate_on_submit():
         user=User.query.filter_by(email=form.email.data).first()
-        print(user)
         if user and bcrypt.check_password_hash(user.password,form.password.data):
             login_user(user,remember=form.remember.data)
             next_page=request.args.get('next')
@@ -102,6 +82,34 @@ def account():
         form.email.data=current_user.email
     image_file=url_for('static',filename='profile_pics/'+current_user.image_file)
     return render_template('account.html',title="Account",image_file=image_file,form=form)
+
+    @app.route("/post/new",methods=['GET','POST'])
+    @login_required
+    def new_post():
+        form=PostForm()
+        if form.validate_on_submit():
+            post=Post(title=form.title.data,content=form.content.data,author-current_user)
+            db.session.add(post)
+            db.session.commit()
+            flash("Your post has been created!", 'success')
+            return redirect(url_for('home'))
+        return render_template('create_post.html',title="New Post",form=form,legend="New Post")
+
+    @app.route("/post/<int:post_id>")
+    def post(post_id):
+        post=Post.query.get_or_404(post_id)
+        return render_template('post.html',title=post.title, post=post)
+
+    @app.route("/post/<int:post_id>/update",methods=["GET","POST"])
+    @login_required
+    def update_post(post_id):
+        post=Post.query.get_or_404(post_id)
+        if post.author1=current_user:
+            abort(403)
+        form=PostForm()
+        if form.validate_on_submit():
+            post.title=form.title.data
+            post.
 
 
 
